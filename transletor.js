81627cc1952d44fe648847829489c12ed09e9285
Deepak Chaurasiya <dchaurasiya8589@gmail.com>
2021-09-09 18:59:55 +0530

    more features added

--- transletor.js
+++ transletor.js
@@ -1,15 +1,31 @@
+setInterval(() => {
+  let text = document.getElementById("box").value;
+  let text2 = document.getElementById("box2").value;
+  if (text.value == "undefined") {
+    text.value = null;
+  }
+  if (text2.value == "undefined") {
+    text2.value = null;
+  }
+}, 100);
+
 function getTheValue() {
   let lang = document.getElementById(`languages`).value;
   localStorage.setItem(`language`, JSON.stringify(lang));
   return lang;
 }
-fetch("https://libretranslate.de/languages")
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-    setValue(res);
-  });
+
+try {
+  fetch("https://libretranslate.de/languages")
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      setValue(res);
+    });
+} catch (err) {
+  console.log("error h");
+}
 
 function setValue(res) {
   res.forEach(({ code, name }) => {
@@ -33,6 +49,14 @@ function translet() {
   let language2 = document.getElementById("language").value;
   result(language, text, language2);
 }
+function translet1() {
+  let language = document.getElementById("languages").value;
+  let text = document.getElementById("box2").value;
+  //    console.log("lan",language)
+  //    console.log(box)
+  let language2 = document.getElementById("language").value;
+  result2(language2, text, language);
+}
 
 async function result(lan, que, lan2) {
   const res = await fetch("https://libretranslate.de/translate", {
@@ -48,6 +72,25 @@ async function result(lan, que, lan2) {
   let translated = await res.json();
   let box2 = document.getElementById("box2");
   box2.value = translated.translatedText;
+}
+async function result2(lan, que, lan2) {
+  try {
+    const res = await fetch("https://libretranslate.de/translate", {
+      method: "POST",
+      body: JSON.stringify({
+        q: `${que}`,
+        source: `${lan2}`,
+        target: `${lan}`,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    let translated = await res.json();
+    let box2 = document.getElementById("box");
+    box2.value = translated.translatedText;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function swap() {
